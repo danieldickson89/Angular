1a1e10100edd4be2ea472349f1eac753a0ffd91b
Daniel Dickson <danieldickson@daniels-mbp.ad.uvu.edu>
2019-01-28 16:54:18 -0700

    adding HTTP requests

--- TourOfHeroes/my-app/src/app/hero-detail/hero-detail.component.ts
+++ TourOfHeroes/my-app/src/app/hero-detail/hero-detail.component.ts
@@ -21,8 +21,8 @@ export class HeroDetailComponent implements OnInit {
 
   ngOnInit() {
     const id = +this.route.snapshot.paramMap.get('id');
-    this.heroService.getHero(id)
-        .subscribe(hero => this.hero = hero);
+    // this.heroService.getHero(id)
+    //     .subscribe(hero => this.hero = hero);
   }
 
   goBack(): void {
