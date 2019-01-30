0cf4b9524f81348bd1a13d61dc8bcb6b2b6d19ef
Daniel Dickson <danieldickson@daniels-mbp.ad.uvu.edu>
2019-01-30 10:37:28 -0700

    fixed mock data structure

--- admin-dashboard-mockup/src/app/in-memory-data.service.ts
+++ admin-dashboard-mockup/src/app/in-memory-data.service.ts
@@ -9,70 +9,86 @@ export class InMemoryDataService {
 
   createDb() {
     const applications = [
-      { name: 'Progress Dashboard',
-        code: 'PRGDB' },
-      { name: 'Instances',
-        code: 'INSTC' }
-    ];
-
-    const categories = [
-      { appCode: 'PRGDB',
-        category: 'FINAID',
-        description: 'Financial Aid' },
-        { appCode: 'PRGDB',
-        category: 'REGIS',
-        description: 'Registration' },
-        { appCode: 'PRGDB',
-        category: 'ADMIS',
-        description: 'Admissions' },
-        { appCode: 'INSTC',
-        category: 'QA',
-        description: 'QA' },
-        { appCode: 'INSTC',
-        category: 'DEV',
-        description: 'Development' },
-        { appCode: 'INSTC',
-        category: 'PROD',
-        description: 'Production' },
-    ];
-
-    const links = [
-      { categCode: 'FINAID',
-        url: 'https://www.uvu.edu/financialaid/',
-        description: 'Blah blah blah' },
-        { categCode: 'REGIS',
-        url: 'https://www.uvu.edu/',
-        description: 'Blah blah blah' },
-        { categCode: 'ADMIS',
-        url: 'https://www.uvu.edu/',
-        description: 'Blah blah blah' },
-        { categCode: 'INSTC',
-        url: 'https://www.uvu.edu/',
-        description: 'Blah blah blah' },
-        { categCode: 'QA',
-        url: 'https://www.uvu.edu/',
-        description: 'Blah blah blah' },
-        { categCode: 'DEV',
-        url: 'https://www.uvu.edu/',
-        description: 'Blah blah blah' },
-        { categCode: 'PROD',
-        url: 'https://www.uvu.edu/',
-        description: 'Blah blah blah' }
-    ]
-
-    const app = {
-      name: 'prog',
-      code: 'something',
-      categories: [
-        {
-          categCode: 'finaid',
-          links: [
-            {link1: '1'},
-            {link2: '2'}
-          ]
+      {
+        name: 'Progress Dashboard',
+        code: 'PRGDB',
+        categories: [
+          {
+            category: 'FINAID',
+            description: 'Financial Aid',
+            links: [
+              {
+                url: 'https://www.uvu.edu/financialaid/',
+                description: 'Financial Aid link description'
+              }
+            ]
+          },
+          {
+            category: 'REGIS',
+            description: 'Registration',
+            links: [
+              {
+                url: 'https://www.uvu.edu/',
+                description: 'Blah blah blah',
+                links: [
+                  {
+                    url: 'https://www.uvu.edu/',
+                    description: 'A link for Registration'
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            category: 'ADMIS',
+            description: 'Admissions',
+            links: [
+              {
+                url: 'https://www.uvu.edu/',
+                description: 'A link for Admissions'
+              }
+            ]
+          }
+        ]
+      },
+      {
+        name: 'Instances',
+        code: 'INSTC',
+        categories: [
+          {
+            category: 'QA',
+            description: 'QA',
+            links: [
+              {
+                url: 'https://www.uvu.edu/',
+                description: 'A link for QA'
+              }
+            ]
+          },
+          {
+            category: 'DEV',
+            description: 'Development',
+            links: [
+              {
+                url: 'https://www.uvu.edu/',
+                description: 'A link for dev'
+              }
+            ]
+          },
+          {
+            category: 'PROD',
+            description: 'Production',
+            links: [
+              {
+                url: 'https://www.uvu.edu/',
+                description: 'A link for production'
+              }
+            ]
+          }
+        ]
       }
-      ]
-    }
-    return { applications, categories, links };
+    ];
+
+    return { applications };
   }
 }
